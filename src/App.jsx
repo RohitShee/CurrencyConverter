a2e28b8fd7366c8ec0a9977c7c04fa3eef8fe705
RohitShee <rohitshee92@gmail.com>
2024-09-17 03:04:35 +0530

    Bug fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import InputBox from './components/InputBox'
 import useCurrencyInfo from './hooks/useCurrencyInfo'
-
+import image from "./assets/image.jpg"
 function App() {
   const [amount,setAmount] = useState(0)
   const [from,setFrom] = useState("usd")
@@ -27,7 +27,7 @@ function App() {
     <div
     className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
     style={{
-        backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`    }}
+        backgroundImage: `url(${image})`    }}
 >
     <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
